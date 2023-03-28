78d16613e05795b176f18db20cb162e27613fe9f
giullianomassarelli <massarelli47@gmail.com>
2023-03-28 11:11:38 -0300

    :sparkles: feat: add swagger

---
+++ src/main/java/br/com/geradordedevs/onlinebank/configurations/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package br.com.geradordedevs.onlinebank.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Value("${swagger.app.name}")
+    private String name;
+
+    @Value("${swagger.app.version}")
+    private String version;
+
+    @Value("${swagger.app.description}")
+    private String description;
+
+    @Value("${swagger.base.package}")
+    private String basePackage;
+
+    @Value("${swagger.contact.name}")
+    private String contactName;
+
+    @Value("${swagger.contact.email}")
+    private String contactEmail;
+
+
+    @Bean
+    public OpenAPI playDeliveriesOpenApi() {
+        return new OpenAPI()
+                .info(new Info().contact(new Contact().name(contactName).email(contactEmail))
+                        .description(description)
+                        .version(version));
+    }
+}
