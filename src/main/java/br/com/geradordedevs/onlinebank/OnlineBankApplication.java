37a0fc506081b86941b172cabbc30cbd5eb1f3fc
giullianomassarelli <massarelli47@gmail.com>
2023-03-28 16:52:38 -0300

    :wrench: Create configs to feign and environment keys

--- src/main/java/br/com/geradordedevs/onlinebank/OnlineBankApplication.java
+++ src/main/java/br/com/geradordedevs/onlinebank/OnlineBankApplication.java
@@ -3,8 +3,9 @@ package br.com.geradordedevs.onlinebank;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-
+@EnableFeignClients
 @SpringBootApplication
 public class OnlineBankApplication {
 
