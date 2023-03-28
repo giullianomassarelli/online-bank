d714314fedc96b04df713dd39d2cefbc55d5b8e4
giullianomassarelli <massarelli47@gmail.com>
2023-03-28 11:00:17 -0300

    :tada: Initial commit

---
+++ src/main/java/br/com/geradordedevs/onlinebank/OnlineBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.geradordedevs.onlinebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankApplication.class, args);
+	}
+
+}
